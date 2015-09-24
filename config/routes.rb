123a9a0307697c53e3a9d6fae3c7ae31f18db638
Manuel Ábalos Serrano <manu.abalos@gmail.com>
2015-09-24 17:18:03 +0200

    Configurando y testeamdo la gema Devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
   
-  devise_for :users
-  root "home#index"
+	#devise_for :users, controllers: { sessions: "users/sessions", registrations: "users/registrations" }
+	devise_for :users
+	root "home#index"
+
+	
   
 end
