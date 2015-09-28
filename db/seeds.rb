dfbf5ae619ff379308031245d7ae14d2540664d0
Manuel Ábalos Serrano <manu.abalos@gmail.com>
2015-09-28 13:24:54 +0200

    Añadidos algunos seeds de prueba para las rutas

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,27 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+Route.create(   name: "Parque del Retiro", 
+				description: "Recorrido por la zona exterior del parque del Retiro.",
+			  	location: "Madrid", 
+			  	difficulty: "Intermedia", 
+			  	distance: 5,
+			  	latitude: 40.416100, 
+			  	longitude: -3.684284)
+
+Route.create( 	name: "Parque del Canal", 
+				description: "Recorrido por la zona exterior del parque del Canal.",
+				location: "Madrid", 
+				difficulty: "Principiantes", 
+				distance: 4,
+				latitude: 40.443223, 
+				longitude: -3.707657)
+
+Route.create( 	name: "Templo de Debod", 
+				description: "Recorrido por la zona del paseo Pintor Rosales y el templo de Debod.",
+			  	location: "Madrid", 
+			  	difficulty: "Principiantes", 
+			  	distance: 5,
+			  	latitude: 40.432529, 
+			  	longitude: -3.725233)
