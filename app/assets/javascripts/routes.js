632af76a8b6f9a184f344e92efc40fae1491c705
Manuel Ábalos Serrano <manu.abalos@gmail.com>
2015-09-28 15:31:59 +0200

    Pequeño testeo con MapBox y añadidas funcionalidades solo en front-end

--- app/assets/javascripts/routes.js
+++ app/assets/javascripts/routes.js
@@ -1,3 +1,50 @@
-# Place all the behaviors and hooks related to the matching controller here.
-# All this logic will automatically be available in application.js.
-# You can use CoffeeScript in this file: http://coffeescript.org/
+$(document).ready(function(){
+
+	L.mapbox.accessToken = 'pk.eyJ1IjoibWFudWFiYWxvcyIsImEiOiJjaWYzdzcyZ3cwMGwwdGZseWVma2IxeXM5In0.oiCfQ9C3Yf7IQJecd3auZg';
+	var geolocate = document.getElementById('geolocate');
+	var map = L.mapbox.map('map', 'mapbox.streets').setView([40.087802, -3.873294], 6);
+
+	var myLayer = L.mapbox.featureLayer().addTo(map);
+
+	// This uses the HTML5 geolocation API, which is available on
+	// most mobile browsers and modern browsers, but not in Internet Explorer
+	//
+	// See this chart of compatibility for details:
+	// http://caniuse.com/#feat=geolocation
+	if (!navigator.geolocation) {
+	    geolocate.innerHTML = 'Geolocation is not available';
+	} else {
+	    geolocate.onclick = function (e) {
+	        e.preventDefault();
+	        e.stopPropagation();
+	        map.locate();
+	    };
+	}
+
+	// Once we've got a position, zoom and center the map
+	// on it, and add a single marker.
+	map.on('locationfound', function(e) {
+	    map.fitBounds(e.bounds);
+
+	    myLayer.setGeoJSON({
+	        type: 'Feature',
+	        geometry: {
+	            type: 'Point',
+	            coordinates: [e.latlng.lng, e.latlng.lat]
+	        },
+	        properties: {
+	            'title': 'Here I am!',
+	            'marker-color': '#ff8888',
+	            'marker-symbol': 'star'
+	        }
+	    });
+
+	});
+
+	// If the user chooses not to allow their location
+	// to be shared, display an error message.
+	map.on('locationerror', function() {
+	    geolocate.innerHTML = 'Position could not be found';
+	});
+
+});
