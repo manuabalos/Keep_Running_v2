9cc31506b59c08aa37a1db6f01bce213d167a5a3
Manuel Ábalos Serrano <manu.abalos@gmail.com>
2015-09-30 18:52:11 +0200

    Añadido el model Waypoint con sus respectivas propiedades

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,12 @@
 Rails.application.routes.draw do
   
 	root "home#index"
-
-	resources :news
-	resources :routes
+	
 	devise_for :users, controllers: { sessions: "users/sessions", registrations: "users/registrations" }
+	resources :news
+	resources :routes do
+		resources :waypoints
+	end
+	
   
 end
---
+++ db/migrate/20150930164321_create_waypoints.rb
@@ -0,0 +1,12 @@
+class CreateWaypoints < ActiveRecord::Migration
+  def change
+    create_table :waypoints do |t|
+    	
+    	t.integer :route_id
+    	t.float :latitude
+    	t.float :longitude
+
+      t.timestamps null: false
+    end
+  end
+end
